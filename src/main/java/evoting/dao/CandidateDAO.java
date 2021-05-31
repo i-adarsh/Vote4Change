5a6ea893e930a9600fe67d12af3c046ea230128f
i-adarsh <i.adarsh.kr@icloud.com>
2021-05-31 22:24:03 +0530

    Last Update

---
+++ src/main/java/evoting/dao/CandidateDAO.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evoting.dao;
+
+import evoting.dbutil.DBConnection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author adarshkumar
+ */
+public class CandidateDAO {
+    private static PreparedStatement ps, ps1, ps2;
+    static{
+        try{
+            ps = DBConnection.getConnection().prepareStatement("Select count(*) from candidate");
+            ps1 = DBConnection.getConnection().prepareStatement("Select username from user_details where aadhar_no=?");
+            ps2 = DBConnection.getConnection().prepareStatement("Select distinct city from user_details");
+        }
+        catch (SQLException ex){
+            ex.printStackTrace();
+        }
+    }
+    
+    public static String getNewId() throws SQLException{
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        return "C"+(100+rs.getInt(1) + 1);
+//        if (rs.next()){
+//            return "C"+(100+rs.getInt(1) + 1);
+//        }
+//        else{
+//            return "C101";
+//        }
+    }
+    
+    public static String getUserNameById(String uid) throws SQLException{
+        ps1.setString(1, uid);
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()){
+            return rs.getString(1);
+        }
+        else{
+            return null;
+        }
+    }
+    
+    public static ArrayList<String> getCity() throws SQLException{
+        ArrayList <String> cityList = new ArrayList<>();
+        ResultSet rs = ps2.executeQuery();
+        while(rs.next()){
+            cityList.add(rs.getString(1));
+        }
+        return cityList;
+    }
+    
+}
